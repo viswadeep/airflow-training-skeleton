0ad79e69e1c172809db10bbb71251ba9ca5101e5
viswadeep <viswadeep@gmail.com>
2020-07-09 10:33:32 -0700

    Create my-exampe.py
    Hello World!

---
+++ dags/my-exampe.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Example DAG demonstrating the usage of the BashOperator."""
+
+from datetime import timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='my-example-01',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+task1 = DummyOperator(task_id="task1", dag=dag)
+task2 = DummyOperator(task_id="task2", dag=dag)
+task3 = DummyOperator(task_id="task3", dag=dag) 
+task4 = DummyOperator(task_id="task4", dag=dag) 
+task5 = DummyOperator(task_id="task5", dag=dag) 
+task6 = DummyOperator(task_id="task6", dag=dag)
+
+
+task1 >> task2 >> [ task3, task4] >> task5 >> task6
